5310bc38ffb0a7dc04957c99d18cc9735b6e5781
TasbeehNaser <tasbeehnaser2002@gmail.com>
2023-11-13 14:48:29 +0200

    editUser

--- src/shared/Input.jsx
+++ src/shared/Input.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Input({id,title,type,name,changeData,error}) {
+function Input({id,title,type,name,changeData,error,value,ifdisabled}) {
   return (
     <div className="mb-3">
     <label htmlFor={id} className="form-label">{title}</label>
-    <input type={type} className="form-control" id={id} name={name} onChange={changeData}/>
+    <input type={type} className="form-control" disabled={ifdisabled} d value={value} id={id} name={name} onChange={changeData}/>
    {error[name]&&<p className='text-danger'>{error[name]}</p>}
   </div>
   )
