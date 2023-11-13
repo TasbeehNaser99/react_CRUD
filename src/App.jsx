86e2836216e2ffdd502343d00a34397de2887bb3
TasbeehNaser <tasbeehnaser2002@gmail.com>
2023-11-13 14:46:45 +0200

    editUser

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Create from './components/users/Create'
 import Index from './components/users/Index'
-import Details from './components/Details'
-
+import Details from './components/users/Details'
+import Edit from './components/users/Edit'
 function App() {
   return (
    <Routes>
@@ -11,6 +11,7 @@ function App() {
     <Route path='/users/:id' element={<Details/>}/>
     <Route path='/users/index' element={<Index/>}/>
     <Route path='/users/create' element={<Create/>}/>
+    <Route path='/users/edit/:id' element={<Edit/>}/>
     <Route path='/*' element={<h2>page Not Found</h2>}/>
    </Routes>
   )
